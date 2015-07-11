5afab1f5d4e57727d5a9ed6c9865a56b7fe0c3a5
Stanislav Zaitsev <staszmail@gmail.com>
2015-07-11 02:23:40 +0200

    changes

---
+++ FlexibleProxy/FlexibleProxy/Networking/IncommingTcpConnection.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using FlexibleProxy.Settings;
+
+namespace FlexibleProxy.Networking
+{
+    public class IncommingTcpConnection : IConnector
+    {
+        private TcpListener Connection { get; set; }
+
+        private Channel ChannelSettings { get; set; }
+
+        public IncommingTcpConnection(Channel channelSetting)
+        {
+            ChannelSettings = channelSetting;
+            Connection = new TcpListener(IPAddress.Any, channelSetting.SourcePort);
+        }
+        
+        public void SendData(byte[] data)
+        {
+            
+        }
+
+        public void Activate()
+        {
+            Connection.Start();
+        }
+
+        public void Deactivate()
+        {
+            Connection.Stop();
+        }
+    }
+}
